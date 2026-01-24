4afc2aebe7cc5554a8a7a502e9117ec205e1c5dc
Sandesh Pulami <sandeshpulami333@gmail.com>
2026-01-24 23:38:46 +0000

    feat: work on connection all the code

--- app/api/notes/route.js
+++ app/api/notes/route.js
@@ -3,29 +3,46 @@ import Note from "@/models/Note";
 
 import { NextResponse } from "next/server";
 
-export async function POST(request){
-    try {
-        await dbConnect();
-        const body = await request.json();
-        const note = await Note.create(body);
+export async function POST(request) {
+  try {
+    await dbConnect();
+    const { title, content } = await request.json();
 
-        return NextResponse.json({success: true, data: note}, {status: 201});
-    } catch (error) {
-        return NextResponse.json(
-            {success: false, error: error.message},
-            {status: 400}
-        );
+    if (!title || !content) {
+      return NextResponse.json(
+        { success: false, error: "Title and content required" },
+        { status: 400 }
+      );
     }
+
+    const note = await Note.create({ title, content });
+
+    return NextResponse.json(
+      { success: true, data: note },
+      { status: 201 }
+    );
+  } catch (error) {
+    return NextResponse.json(
+      { success: false, error: error.message },
+      { status: 400 }
+    );
+  }
 }
 
-export async function GET(){
-    try {
-        await dbConnect();
-        const notes = await Note.find({}).sort({createdAt: -1});
-    } catch (error) {
-        return NextResponse.json(
-            {success: false, error: error.message},
-            {status: 400}
-        );
-    }
+
+export async function GET() {
+  try {
+    await dbConnect();
+    const notes = await Note.find({}).sort({ createdAt: -1 });
+
+    return NextResponse.json(
+      { success: true, data: notes },
+      { status: 200 }
+    );
+  } catch (error) {
+    return NextResponse.json(
+      { success: false, error: error.message },
+      { status: 400 }
+    );
+  }
 }
--- components/NotesClient.jsx
+++ components/NotesClient.jsx
@@ -1,12 +1,66 @@
 "use client"
-import React from 'react'
+import React, { useState } from 'react'
 
 const NotesClient = () => {
-  return (
-    <div>
-      NotesClient bro
-    </div>
-  )
+    const [title, setTitle] = useState("");
+    const [content, setContent] = useState("");
+    const [loading, setLoading] = useState(false);
+
+    const createNote = async(e) => {
+        e.preventDefault();
+        if(!title.trim() || !content.trim()) return;
+        setLoading(true);
+
+        try {
+            const response = await fetch("/api/notes", {
+                method: "POST",
+                headers:{"Content-Type": "application/json"},
+                body:JSON.stringify({title, content})
+            });
+
+            const result = await response.json();
+            console.log(result);
+            setLoading(false);
+
+        } catch (error) {
+            console.error("Error creating error", error);
+        }
+    }
+    return (
+        <div className="space-y-6">
+            <form className="bg-white p-6 rounded-lg shadow-md" onSubmit={createNote}>
+                <h2 className="text-xl font-semibold">Create new Note</h2>
+
+                <div className="space-y-4">
+                    <input
+                        type="text"
+                        placeholder="Title"
+                        value={title}
+                        onChange={(e) => setTitle(e.target.value)}
+                        className="w-full p-3 border border-gray-300 rounded-md"
+                        required
+                    />
+
+                    <textarea
+                        placeholder="Note Content"
+                        value={content}
+                        onChange={(e) => setContent(e.target.value)}
+                        rows={4}
+                        className="w-full p-3 border border-gray-300 rounded-md"
+                    />
+
+                    <button
+                        type="submit"
+                        disabled={loading}
+                        className="bg-blue-500 text-white px-6 py-2 rounded-md hover:bg-blue-700"
+                    >
+                        {loading ? "Creating..." : "Create Note"}
+                    </button>
+                </div>
+            </form>
+        </div>
+
+    )
 }
 
 export default NotesClient
