9009d5082844c5600923a69540f1dc9c799f4771
Sandesh Pulami <sandeshpulami333@gmail.com>
2026-01-24 22:43:18 +0000

    feat: add a get request

--- app/api/notes/route.js
+++ app/api/notes/route.js
@@ -1,2 +1,31 @@
 import dbConnect from "@/lib/db";
+import Note from "@/models/Note";
 
+import { NextResponse } from "next/server";
+
+export async function POST(request){
+    try {
+        await dbConnect();
+        const body = await request.json();
+        const note = await Note.create(body);
+
+        return NextResponse.json({success: true, data: note}, {status: 201});
+    } catch (error) {
+        return NextResponse.json(
+            {success: false, error: error.message},
+            {status: 400}
+        );
+    }
+}
+
+export async function GET(){
+    try {
+        await dbConnect();
+        const notes = await Note.find({}).sort({createdAt: -1});
+    } catch (error) {
+        return NextResponse.json(
+            {success: false, error: error.message},
+            {status: 400}
+        );
+    }
+}
